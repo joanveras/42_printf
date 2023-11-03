9be501505aa820af625e3eaa4eee8d9cc8e462c0
jveras <verasjoan587@gmail.com>
2023-11-03 13:37:09 -0300

    "libft" renamed to "lib42"

--- libftprintf/libftprintf.h
+++ libftprintf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jveras <jveras@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 08:57:23 by jveras            #+#    #+#             */
-/*   Updated: 2023/11/03 11:55:47 by jveras           ###   ########.fr       */
+/*   Updated: 2023/11/03 13:35:33 by jveras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define LIBFTPRINTF_H
 
 # include <stdarg.h>
-# include "../libft/libft.h"
+# include "../lib42/libft.h"
 
 void	lower_hex(unsigned int n);
 void	upper_hex(unsigned int n);
